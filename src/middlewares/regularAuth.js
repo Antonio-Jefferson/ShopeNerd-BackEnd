32070af8fdb77904a5d2d2ee6eaca0704f42c8c4
GustavoCMatias <gustavocmatias@gmail.com>
2023-01-29 13:28:50 -0300

    fix/corrected put products

--- src/middlewares/regularAuth.js
+++ src/middlewares/regularAuth.js
@@ -1,18 +1,17 @@
 import db from "../database/db.js"
 
-export default function regularAuth() {
-    return async (req, res, next) => {
-        const {authorization} = req.headers
-        const token = authorization?.replace("Bearer ", "")
-        if(!token) return res.status(422).send("informe o token!")
-        try {
-            const userInfo = await db.collection('sessoes').findOne({ token })
-            if (!userInfo) return res.status(401).send("Acesso não autorizado!")
-            next()
-        } catch (err) {
-            console.log(err);
-            return res.status(500).send("Database error.");
-        }
-        next();
+export default async function regularAuth(req, res, next) {
+    const { authorization } = req.headers
+    const token = authorization?.replace("Bearer ", "")
+    if (!token) return res.status(422).send("informe o token!")
+    try {
+        const userInfo = await db.collection('sessions').findOne({ token })
+        if (!userInfo) return res.status(401).send("Acesso não autorizado!")
+        next()
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send("Database error.");
     }
+    next();
 }
+
